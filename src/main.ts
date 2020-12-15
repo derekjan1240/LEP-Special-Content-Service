fbaa2f5a29a9f0a396391f2fce5f737c2718894b
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2020-12-15 18:53:05 +0800

    add starting log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8084);
+  await app.listen(8084, () => {
+    Logger.log('Special Content Service is running on port 8084!');
+  });
 }
 bootstrap();
